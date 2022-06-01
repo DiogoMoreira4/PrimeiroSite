509cc2ae35b6c86cc276b725a567e5e405280bf1
unknown <diogo.silva2607@gmail.com>
2022-06-01 15:34:05 +0100

    Redirecionamento com Link

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
+import Link from 'next/link';
+
 function Home(){
-    return <div>
+    return (<div>
         <h1>Meu Primeiro Site</h1>
         <h2>Teste1</h2>
-        </div>
+        <Link href="/sobre">
+            <a>Visitar a página Sobre</a> 
+        </Link>
+        </div>)
 }
 
 export default Home
