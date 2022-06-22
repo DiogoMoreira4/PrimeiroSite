0d16290322c4cc92e3c818079984ea91a5efa20a
unknown <diogo.silva2607@gmail.com>
2022-06-22 23:35:55 +0100

    Normal

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,6 @@ export default function Home(){
     return (
     <div>
         <title>Primeiro Site</title>
-        <h1>Mensagem para a Kina:</h1>
-        <h1>Vai ver se está a chover, eu não vou ao Pingo Doce</h1>
         <Link href="/sobre">
             <a>Visitar a página Sobre</a> 
         </Link>
