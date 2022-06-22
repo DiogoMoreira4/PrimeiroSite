d903e3df2321a42f64d486aaec351d25757b3ffe
unknown <diogo.silva2607@gmail.com>
2022-06-22 23:26:49 +0100

    Experiencia

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import Link from 'next/link';
 
-function Home(){
-    return (<div>
-        <h1>Meu Primeiro Site</h1>
-        <h2>Teste1</h2>
+
+export default function Home(){
+    return (
+    <div>
+        <title>Primeiro Site</title>
         <Link href="/sobre">
             <a>Visitar a página Sobre</a> 
+            <a>Vai ver se está a chover, eu não vou ao Pingo Doce</a>
         </Link>
-        </div>)
-}
 
-export default Home
+    </div>)
+}
--- pages/produtos/[id].js
+++ pages/produtos/[id].js
@@ -1,3 +1,4 @@
+
 export async function getStaticPaths(){
     return {
         paths:[{
