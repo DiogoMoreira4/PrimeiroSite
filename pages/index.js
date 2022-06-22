f8f435d84338d30c64c71d61c242fcbbc704c7b5
unknown <diogo.silva2607@gmail.com>
2022-06-22 23:33:21 +0100

    Experiencia_2

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,10 @@ export default function Home(){
     return (
     <div>
         <title>Primeiro Site</title>
+        <h1>Mensagem para a Kina:</h1>
+        <h1>Vai ver se está a chover, eu não vou ao Pingo Doce</h1>
         <Link href="/sobre">
             <a>Visitar a página Sobre</a> 
-            <h1>Vai ver se está a chover, eu não vou ao Pingo Doce</h1>
         </Link>
-
     </div>)
 }
